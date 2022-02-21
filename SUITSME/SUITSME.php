a829d63173b4c6405f33fa3e8d1f69776bf05d39
Brett Shirley <82629580+Brshirl@users.noreply.github.com>
2022-02-21 15:24:06 -0600

    Add files via upload

---
+++ SUITSME/SUITSME.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SUITS ME</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+     body{ font: 16px sans-serif; text-align: center; background-color: pink;}
+     a{
+            text-decoration: none;
+            color: red;
+            font-size: 20px;
+            padding: 8px;
+        }
+        a:hover{
+            color: black;
+        }
+             h1{
+        font: 80px Fantasy;
+        text-align: center;
+        text-shadow: 2px 2px 5px grey;
+        color: black;
+        
+        }
+        h2{
+            font: 45px sans-serif;
+            text-align: center;
+            color: pink;
+            background-color: black;
+           
+          }
+        h3{
+            font: 40px sans-serif;
+            text-align: center;
+            color: black;
+
+          }
+        nav{
+            text-align: center;
+        }
+     
+     footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    color: black;
+    text-align: center;
+            }
+
+           
+.column {
+  float: left;
+  width: 100%;
+  padding: 5px;
+}
+
+/* Clear floats after image containers */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+            
+
+    </style>
+</head>
+<header id= "header" style= "display: block"; >
+<hgroup>
+    <h1 class = "page-logo">
+        <h1>BBQ</h1>
+        </h1>
+        <h2 class = "page-logo1">
+        <h2> SUITS ME </h2>
+        </h2>
+        <h3 class = page-logo2>
+        <h3> Mobile Smoker </h3>
+        </h3>
+        </header>
