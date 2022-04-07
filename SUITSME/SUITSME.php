1108dc651ff7b2ecd538b5fce4d7734d9312ebc6
Brett Shirley <82629580+Brshirl@users.noreply.github.com>
2022-04-07 17:47:00 -0500

    Update SUITSME.php

--- SUITSME/SUITSME.php
+++ SUITSME/SUITSME.php
@@ -5,7 +5,7 @@
     <title>SUITS ME</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-     body{ font: 16px sans-serif; text-align: center; background-color: pink;}
+     body{ font: 22px sans-serif; text-align: center; background-color: pink;}
      a{
             text-decoration: none;
             color: red;
@@ -35,6 +35,7 @@
             color: black;
 
           }
+
         nav{
             text-align: center;
         }
@@ -68,12 +69,42 @@
 <header id= "header" style= "display: block"; >
 <hgroup>
     <h1 class = "page-logo">
-        <h1>BBQ</h1>
+        <h1>Mobile Smoker BBQ</h1>
         </h1>
         <h2 class = "page-logo1">
         <h2> SUITS ME </h2>
         </h2>
-        <h3 class = page-logo2>
-        <h3> Mobile Smoker </h3>
-        </h3>
         </header>
+  <body>
+<p1>
+  <b>SuitsMe</b> is a BBQ food truck located out of Trenton Illinois. Owners James & Sarah Vastine have been operating since 2021.
+  <br>They can provide for all of your BBQ needs, dont forget they do unique desserts and sweets too!</br>
+   SuitsMe will be up and smoking at the start of the summer. <br></br>
+</p1>
+
+<p>
+  Caterings
+</p>
+<p>
+  Carnival's   
+</p>
+<p>
+    Parties  
+</p>
+<p>
+  Special Events
+</p>
+
+<p>
+<img  align= "left" src = "https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/165624511_3796030990523040_7797950620964592590_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=f5p1DVS14Q4AX-aEE2b&_nc_ht=scontent-ort2-1.xx&oh=00_AT8TsmYvT__OcOJ_tjRciFVjzQYYPV6eQzS6omrfDARUKw&oe=626A1DAD" 
+alt= "SuitsME LOGO" width="600" height="700" >
+</p>
+</body>
+<img align="right" src = "https://scontent-ort2-1.xx.fbcdn.net/v/t1.6435-9/186532356_3924379074354897_7730375700798978110_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=k1XCERyU2kQAX-dqlQb&_nc_ht=scontent-ort2-1.xx&oh=00_AT_Z0GD5GK_87Bqfir2J5enBAPnHZvTbAWgxGr-zpToJkA&oe=6273DA4F"
+alt= "SuitsMe Menu" width = "600" height="700" >
+
+
+<footer>
+<p> Copyright 2022. SuitsME Rights Reserved </p>
+</footer>
+</html>
